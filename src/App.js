3a4ac21bbcbc42cb0291dcfab3e0b7c8fa62add5
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-07-02 03:29:19 +0530

    initial frontend

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+    <h1 class="headingMain display">NFTChat</h1> 
+      <div class="main-line"> 
+          <div class="text"> 
+            <h1 class="text-nft">Revolutionary new way to mint NFTs</h1> 
+            <p class="subtext-nft">Chat with our automated chat-bot to understand Web3 and start your minting journey!</p> 
+          </div> 
+      </div>
     </div>
   );
 }
