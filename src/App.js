f7523b585d5bea163966bbeaad6c4fb9439c9b62
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-07-02 03:58:45 +0530

    css fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1 class="headingMain display">NFTChat</h1> 
-      <div class="main-line"> 
-          <div class="text"> 
-            <h1 class="text-nft">Revolutionary new way to mint NFTs</h1> 
-            <p class="subtext-nft">Chat with our automated chat-bot to understand Web3 and start your minting journey!</p> 
-          </div> 
+      <h1 class="headingMain display">NFTChat</h1>
+      <div class="main-line">
+        <div class="text">
+          <h1 class="text-nft">Revolutionary new way to mint NFTs</h1>
+          <p class="subtext-nft-main">
+            Chat with our automated chat-bot to understand Web3 and start your
+            minting journey!
+          </p>
+        </div>
+      </div>
+      <div class="main-next-line">
+        <div class="text">
+          <h1 class="text-nft-sub">Click on the <strong>Blue Chat Button</strong> on the bottom right!</h1>
+          <p class="subtext-nft">
+            Our services include Minting NFTs on 14 different chains, IPFS
+            storage and ability to ask questions related to web3!
+          </p>
+        </div>
+      </div>
+
+      <div class="main-next-next-line">
+        <div class="text">
+          <h1 class="text-nft-sub">Did You Do It?</h1>
+          <p class="subtext-nft">
+            We offer powers of Generative AI and ChatGPT-4 in our chatbot.
+          </p>
+        </div>
       </div>
     </div>
   );
